f5f4bfc969bd98bd364067e63cfde3643bc64efe
lyndsaywatts <50735031+lyndsaywatts@users.noreply.github.com>
2019-05-31 10:07:24 +0100

    Completed the module

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -65,6 +65,9 @@ public class ControllerServlet extends HttpServlet {
 				case "/edit":
 					showEditForm(request, response);
           break;
+				case "/update":
+					updateBook(request, response);
+          break;
         default:
 				   listBooks(request, response);
            break;
@@ -128,6 +131,17 @@ public class ControllerServlet extends HttpServlet {
 		out.println("This is the doPost() method!");
 		doGet(request, response);
 
+	}
+	
+	private void updateBook(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		int id= Integer.parseInt(request.getParameter("id"));
+		String title= request.getParameter("booktitle");
+		String author= request.getParameter("bookauthor");
+		String price= request.getParameter("bookprice");
+		Book updatedBook= new Book(id,title,author,Float.parseFloat(price));
+		bookDAO.updateBook(updatedBook);
+		response.sendRedirect("list");
 	}
 	
 	private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
